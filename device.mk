f7fa2b1fce5904d5e8804461fc292b6610df794f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-17 00:07:31 +0530

    [TEMP] ginkgo: Switch back to AOSP NFC stack

--- device.mk
+++ device.mk
@@ -59,16 +59,11 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/nxp/opensource/sn100x
 
 PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    com.gsma.services.nfc \
-    com.nxp.nfc.nq \
-    libnqnfc_nci_jni \
-    nfc_nci.nqx.default.hw \
-    NQNfcNci \
-    nqnfcee_access.xml \
-    nqnfcse_access.xml \
+    android.hardware.nfc@1.2-service \
+    android.hardware.secure_element@1.0-service \
+    NfcNci \
     Tag \
-    vendor.nxp.hardware.nfc@1.2-service
+    SecureElement
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
